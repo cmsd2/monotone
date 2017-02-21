ed0b7d75ab804ccc03f3ef80ab4ef14ffff45d13
Chris Dawes <cmsd2@cantab.net>
2017-02-21 09:05:32 +0000

    add unit tests for local impl

--- monotone/src/local/counter.rs
+++ monotone/src/local/counter.rs
@@ -33,3 +33,25 @@ impl MonotonicCounter for Counter {
         Ok(*counter)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use ::*;
+    use super::*;
+
+    #[test]
+    pub fn test_counter_get() {
+        let c = Counter::new();
+        assert_eq!(0, c.get_value().expect("get"));
+    }
+
+    #[test]
+    pub fn test_counter_next() {
+        let c = Counter::new();
+        assert_eq!(1, c.next_value().expect("next"));
+        assert_eq!(1, c.get_value().expect("get"));
+
+        assert_eq!(2, c.next_value().expect("next"));
+        assert_eq!(2, c.get_value().expect("get"));
+    }
+}
--- monotone/src/local/queue.rs
+++ monotone/src/local/queue.rs
@@ -35,12 +35,16 @@ impl QueueInner {
     }
 
     fn join_queue(&mut self, process_id: String) -> Result<(u64, Ticket)> {
+        if let Ok((ft, ticket)) = self.get_ticket(&process_id) {
+            return Ok((ft, ticket));
+        }
+
         self.version += 1;
 
         let position = self.items.len();
         let counter = self.counter;
         let ticket = QueueTicket::new(process_id.clone(), counter);
-        
+
         self.items.push(ticket);
         self.counter += 1;
 
@@ -115,5 +119,142 @@ impl MonotonicQueue for Queue {
         let inner = self.items.lock().unwrap();
 
         inner.get_tickets()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use ::*;
+    use string::*;
+    use super::*;
+
+    /*
+    interesting test cases:
+    for each initial condition in [no ticket in queue, ticket in queue]:
+        Queue::get_ticket()
+        Queue::get_tickets()
+        Queue::join()
+        Queue::leave() after join
+        Queue::leave() before join
+    */
+
+    #[test]
+    pub fn test_queue_no_row_get() {
+        let q = Queue::new();
+        assert!(q.get_ticket("foo").is_err());
+    }
+
+    #[test]
+    pub fn test_queue_row_get() {
+        let q = Queue::new();
+        let (ft, tok) = q.join_queue(s("foo")).expect("join");
+        assert_eq!(ft, 1);
+        assert_eq!(&tok.process_id, "foo");
+        assert_eq!(tok.counter, 0);
+        assert_eq!(tok.position, 0);
+
+        let (ft, tok2) = q.get_ticket("foo").expect("get");
+        assert_eq!(ft, 1);
+        assert_eq!(tok2, tok);
+    }
+
+    #[test]
+    pub fn test_queue_no_row_get_all() {
+        let q = Queue::new();
+        let (ft, toks) = q.get_tickets().expect("get all");
+        assert_eq!(ft, 0);
+        assert_eq!(toks, vec![]);
+    }
+
+    #[test]
+    pub fn test_queue_row_get_all() {
+        let q = Queue::new();
+        let (ft, tok) = q.join_queue(s("foo")).expect("join");
+        assert_eq!(ft, 1);
+        assert_eq!(&tok.process_id, "foo");
+        assert_eq!(tok.counter, 0);
+        assert_eq!(tok.position, 0);
+
+        let (ft, toks) = q.get_tickets().expect("get all");
+        assert_eq!(ft, 1);
+        assert_eq!(toks, vec![tok]);
+    }
+
+    #[test]
+    pub fn test_queue_no_row_join() {
+        let q = Queue::new();
+        let (ft, tok) = q.join_queue(s("foo")).expect("join");
+        assert_eq!(ft, 1);
+        assert_eq!(&tok.process_id, "foo");
+        assert_eq!(tok.counter, 0);
+        assert_eq!(tok.position, 0);
+    }
+
+    #[test]
+    pub fn test_queue_same_row_join() {
+        let q = Queue::new();
+        let (ft, tok) = q.join_queue(s("foo")).expect("join");
+        assert_eq!(ft, 1);
+        assert_eq!(&tok.process_id, "foo");
+        assert_eq!(tok.counter, 0);
+        assert_eq!(tok.position, 0);
+
+        let (ft, tok2) = q.join_queue(s("foo")).expect("join");
+        assert_eq!(ft, 1);
+        assert_eq!(tok2, tok);
+    }
+
+    #[test]
+    pub fn test_queue_different_row_join() {
+        let q = Queue::new();
+        let (ft, tok) = q.join_queue(s("foo")).expect("join");
+        assert_eq!(ft, 1);
+        assert_eq!(&tok.process_id, "foo");
+        assert_eq!(tok.counter, 0);
+        assert_eq!(tok.position, 0);
+
+        let (ft, tok) = q.join_queue(s("bar")).expect("join");
+        assert_eq!(ft, 2);
+        assert_eq!(&tok.process_id, "bar");
+        assert_eq!(tok.counter, 1);
+        assert_eq!(tok.position, 1);
+    }
+
+    #[test]
+    pub fn test_queue_no_row_leave() {
+        let q = Queue::new();
+        assert!(q.leave_queue("foo").is_err());
+    }
+
+    #[test]
+    pub fn test_queue_row_leave() {
+        let q = Queue::new();
+        let (ft, _tok) = q.join_queue(s("foo")).expect("join");
+        assert_eq!(ft, 1);
+
+        let ft = q.leave_queue("foo").expect("leave");
+        assert_eq!(ft, 2);
+    }
+
+    #[test]
+    pub fn test_queue_get_after_leave() {
+        let q = Queue::new();
+        let (ft, tok) = q.join_queue(s("foo")).expect("join");
+        assert_eq!(ft, 1);
+        assert_eq!(tok.position, 0);
+  
+        let (ft, tok) = q.join_queue(s("bar")).expect("join");
+        assert_eq!(ft, 2);
+        assert_eq!(tok.position, 1);
+
+        let ft = q.leave_queue("foo").expect("leave");
+        assert_eq!(ft, 3);
+
+        let (ft, tok) = q.get_ticket("bar").expect("get");
+        assert_eq!(ft, 3);
+        assert_eq!(tok.position, 0);
+        assert_eq!(&tok.process_id, "bar");
+        assert_eq!(tok.counter, 1);
     }
 }
